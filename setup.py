3dedc38c21afd855374b3b4b44d87784d63d4eba
Solumilken <s916526000@gmail.com>
2019-05-10 14:50:00 +0800

    modified packages in setup

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
-from setuptools import setup
+from setuptools import setup, find_packages
+
 
 readme = Path(__file__).parent.joinpath('README.md')
 if readme.exists():
@@ -9,15 +10,14 @@ if readme.exists():
 else:
     long_description = '-'
 
+
 setup(
     name='yoctol-argparse',
     version='0.1.0',
     description='Argument Parser create by Yoctol',
     long_description=long_description,
     python_requires='>=3.6',
-    packages=[
-        'yoctol-argparse',
-    ],
+    packages=find_packages(),
     author='noobOriented',
     author_email='jsaon@yoctol.com',
     url='',
